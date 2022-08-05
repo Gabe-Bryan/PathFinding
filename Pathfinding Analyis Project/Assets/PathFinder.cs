866f0912d6a670359de603c1d6ea5ad82e86f75e
Gabe-Bryan <100544375+Gabe-Bryan@users.noreply.github.com>
2022-08-05 16:58:45 -0700

    Creating Repo

---
+++ Pathfinding Analyis Project/Assets/PathFinder.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Graph;
+using static Graph.Node;
+
+public class PathFinder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateGraph();
+    }
+
+    private Graph CreateGraph() {
+        Node[] nodes = new Node[13];
+        for (int i = 0; i < nodes.Length; i++) {
+            nodes[i] = new Node();
+        }
+        //s=0, a=1, b=2, ... , l = 12
+        ArrayList nodeList = new ArrayList(nodes);
+        Graph graph = new Graph(nodeList);
+
+        //S edges
+        nodes[0].edges.Add(new Edge(nodes[1], 7));
+        nodes[0].edges.Add(new Edge(nodes[2], 2));
+        nodes[0].edges.Add(new Edge(nodes[3], 3));
+
+        //A Edges
+        nodes[1].edges.Add(new Edge(nodes[2], 3));
+        nodes[1].edges.Add(new Edge(nodes[0], 7));
+        nodes[1].edges.Add(new Edge(nodes[4], 4));
+
+        //B Edges
+        nodes[2].edges.Add(new Edge(nodes[0], 2));
+        nodes[2].edges.Add(new Edge(nodes[1], 3));
+        nodes[2].edges.Add(new Edge(nodes[4], 4));
+        nodes[2].edges.Add(new Edge(nodes[8], 1));
+
+        //C Edges
+        nodes[3].edges.Add(new Edge(nodes[0], 3));
+        nodes[3].edges.Add(new Edge(nodes[12], 2));
+
+        //D Edges
+        nodes[4].edges.Add(new Edge(nodes[1], 4));
+        nodes[4].edges.Add(new Edge(nodes[2], 4));
+        nodes[4].edges.Add(new Edge(nodes[6], 5));
+
+        //E Edges
+        nodes[5].edges.Add(new Edge(nodes[11], 5));
+        nodes[5].edges.Add(new Edge(nodes[7], 2));
+
+        //F Edges
+        nodes[6].edges.Add(new Edge(nodes[4], 5));
+        nodes[6].edges.Add(new Edge(nodes[8], 3));
+
+        //G Edges
+        nodes[7].edges.Add(new Edge(nodes[5], 2));
+        nodes[7].edges.Add(new Edge(nodes[8], 2));
+
+        //H Edges
+        nodes[8].edges.Add(new Edge(nodes[2], 1));
+        nodes[8].edges.Add(new Edge(nodes[6], 3));
+        nodes[8].edges.Add(new Edge(nodes[7], 2));
+
+        //I Edges
+        nodes[9].edges.Add(new Edge(nodes[10], 6));
+        nodes[9].edges.Add(new Edge(nodes[11], 4));
+        nodes[9].edges.Add(new Edge(nodes[12], 4));
+
+        //J Edges
+        nodes[10].edges.Add(new Edge(nodes[9], 6));
+        nodes[10].edges.Add(new Edge(nodes[11], 4));
+        nodes[10].edges.Add(new Edge(nodes[12], 4));
+
+        //K Edges
+        nodes[11].edges.Add(new Edge(nodes[5], 5));
+        nodes[11].edges.Add(new Edge(nodes[9], 4));
+        nodes[11].edges.Add(new Edge(nodes[10], 4));
+
+        //L Edges
+        nodes[12].edges.Add(new Edge(nodes[3], 2));
+        nodes[12].edges.Add(new Edge(nodes[9], 4));
+        nodes[12].edges.Add(new Edge(nodes[10], 4));
+
+        //Debug.Log(((Node)nodeList[0]).edges.Count);
+        LinkedList<Node> pathChosen = new LinkedList<Node>();
+        Debug.Log("cost: " + graph.FindShortestPath(nodes[0], nodes[5], pathChosen));
+
+        return graph;
+    }
+}
---
+++ Pathfinding Analyis Project/Assets/Scripts/Graph.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Graph.Node;
+
+public class Graph
+{
+    public class Node 
+    {
+        public class Edge 
+        {
+            public Node neighbor;
+            public int weight;
+            public Edge(Node _neighbor, int _weight) {
+                neighbor = _neighbor; weight = _weight;
+            }
+        }
+
+        public ArrayList edges;
+        public Node() {
+            edges = new ArrayList();
+        }
+    }
+
+    ArrayList nodes = new ArrayList();
+
+    public Graph(ArrayList startingNodes) {
+        nodes = startingNodes;
+    }
+
+    /**
+     * @param path is an out variable for storing the chosen path.
+     */
+    public int FindShortestPath(Node start, Node end, LinkedList<Node> path) {
+        //TODO: Replace hashtables with min heaps
+        if (start == end) return 0;
+        //initialize correct shortest path set (empty)
+        Dictionary<Node, int> shortP = new Dictionary<Node, int>();
+        //initialize estimated shortest path set (empty)
+        Dictionary<Node, int> estP = new Dictionary<Node, int>();
+        //initialize start to have length 0 and add it to correct
+        shortP.Add(start, 0);
+        Relax(start, 0, shortP, estP);
+        //loops until cost is determined for end node
+        while (!shortP.ContainsKey(end)) {
+            Debug.Log("Started while loop");
+            //find an element t with the least estimate in the estimate set
+            KeyValuePair<Node, int> smallestCost = new KeyValuePair<Node, int>(null, -1);
+            foreach (KeyValuePair<Node, int> v in estP) {
+                if (smallestCost.Key == null || v.Value < smallestCost.Value) smallestCost = v;
+            }
+            shortP.Add(smallestCost.Key, smallestCost.Value);
+            estP.Remove(smallestCost.Key);
+            Relax(smallestCost.Key, smallestCost.Value, shortP, estP);
+            //add t to correct shortest path
+            //Relax(t)
+        }
+        int rValue;
+        bool success = shortP.TryGetValue(end, out rValue);
+        return rValue;
+    }
+
+    private void Relax(Node vertex, int vCost, Dictionary<Node, int> shortP, Dictionary<Node, int> estP) {
+
+        foreach (Edge edge in vertex.edges) {
+            if (!shortP.ContainsKey(edge.neighbor)) {
+                int estCost = vCost + edge.weight;
+                int origCost;
+                if (!estP.TryGetValue(edge.neighbor, out origCost)) {
+                    estP.Add(edge.neighbor, estCost);
+                } else if(origCost > estCost){
+                    estP[edge.neighbor] = estCost;
+                }
+            }
+        }
+    }
+}
